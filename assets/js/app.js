58b6d10ec7ccc0d987d759cd5f7e15a19b47b3c4
Caspar Landman <info@casparlandman.nl>
2025-01-18 01:55:29 +0100

    [/] Hotfix

--- assets/js/app.js
+++ assets/js/app.js
@@ -411,3 +411,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Call toggleSortable initially and whenever the list is updated
     toggleSortable();
 });
+
+
+// Dynamic favicon //
+window.onload = function () {
+	const favicon = document.getElementById('favicon')
+  
+	document.addEventListener('visibilitychange', function (e) {
+	  const isPageActive = !document.hidden
+	  toggle(isPageActive)
+	})
+  
+	function toggle(isPageActive) {
+	  if (isPageActive) {
+		favicon.href = '../assets/images/logo.png'
+	  } else {
+		favicon.href = '../assets/images/logo-away.png'
+	  }
+	}
+}
